3fbd8c1ff2502add76d6164e395e18cdf669ca9b
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-10-28 13:25:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите температуру в градусах Цельсия: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double f = c * 9 / 5 + 32;
+            Console.WriteLine("Температура по Фаренгейту равна: {0}", f);
+            Console.ReadKey();
+        }
+    }
+}
